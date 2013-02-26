d956a2d9261bf0c4526e544348585587a46a76bb
Ly <lxn2@uw.edu>
2013-02-26 12:11:04 -0800

    RMSE & interface
    -RMSE calculates rootmeansquare of all instantians of W(queried
    variables)
    -Cleaner interface
    -Gibbs not working

--- BayesNets/src/cpsc470/bayes/BayesNetRunner.java
+++ BayesNets/src/cpsc470/bayes/BayesNetRunner.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -23,6 +24,7 @@ import aima.core.probability.bayes.exact.EliminationAsk;
 import aima.core.probability.bayes.exact.EnumerationAsk;
 import aima.core.probability.bayes.impl.BayesNet;
 import aima.core.probability.bayes.util.XMLBIFSAXParser;
+import aima.core.probability.domain.Domain;
 import aima.core.probability.proposition.AssignmentProposition;
 import aima.test.core.unit.probability.bayes.util.XMLBIFSAXParserTest;
 
@@ -41,9 +43,85 @@ public class BayesNetRunner {
 		}
 		String filePath = args[0];
 		BayesNet bayesNet = importNetwork(filePath);
+		
+		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+		List<CategoricalDistribution> distributionList = new LinkedList<CategoricalDistribution>();
+		boolean loop = true;
 		//XMLBIFSAXParserTest test = new XMLBIFSAXParserTest();
 		//test.testLoadAlarmNetwork();
+		while (loop){
+			
+			System.out.println("1. Run Inference" );
+			System.out.println("2. Check RMSE" );
+			System.out.println("3. Quit" );
+			System.out.println("Input selection of choice you want to run" );
+					
+			String input = null;
+			int choice = 0;
+			try {
+				input = bufferRead.readLine();
+				choice = Integer.parseInt(input);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			
+			if(choice == 1){
+				distributionList.add(runInference(bayesNet) );
+			}else if(choice== 2){
+				runRMSE(distributionList);
+					
+				
+			}else if(choice== 3){
+				loop = false;
+			}
+			
+		}
+
 		
+		
+		
+	}
+	
+	private static void runRMSE(List<CategoricalDistribution> distributionList){
+		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("");
+		for(int i=0; i<distributionList.size(); i++){
+			CategoricalDistribution temp = distributionList.get(i);
+			System.out.println(i + " " +  temp.toString()   );			
+		}
+		System.out.println(distributionList.size() + " Quit");
+		
+		
+		System.out.println("Separated by commas, input indexes of distributions you want to run RMSE on");
+		String delims = "[,]";
+		String input = null;
+		
+		try {
+			input = bufferRead.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String[] tokens = input.split(delims);
+		int distribLocation1 = Integer.parseInt(tokens[0]);
+		if(distribLocation1 != distributionList.size() && tokens.length >= 1 ){
+			
+			int distribLocation2 = Integer.parseInt(tokens[1]);
+			
+			double rmse = Utils.computeDistributionRMSE(distributionList.get(distribLocation1),
+															distributionList.get(distribLocation2) );
+
+			System.out.println("RMSE: " + rmse);
+			System.out.println();
+		
+		}
+		
+	}
+	private static CategoricalDistribution runInference(BayesNet bayesNet){
 		List<RandomVariable> vars = new ArrayList<RandomVariable>();
 		
 		vars = bayesNet.getVariablesInTopologicalOrder();
@@ -62,11 +140,8 @@ public class BayesNetRunner {
 		System.out.println("Exact run time using " + bayesInference.getClass().getSimpleName() + ": " + runTimeMillis + "s");
 		System.out.println();
 		
-		
-		
-		
+		return targetDistrib;
 	}
-	
 	private static RandomVariable[]  convertQuerry(List<RandomVariable> queryVars) {
 		RandomVariable[] queryVariables = new RandomVariable[queryVars.size()];
 		for (int i =0; i< queryVars.size(); i++){
@@ -82,7 +157,7 @@ public class BayesNetRunner {
 		return evidenceList;
 	}
 	private static BayesInference getAlgoType(){
-		Scanner scanner = new Scanner(System.in);
+		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
 		BayesInference algorithm = null;
 		System.out.println("1. Enumeration" );
 		System.out.println("2. Variable elimination" );
@@ -94,29 +169,42 @@ public class BayesNetRunner {
 		System.out.println("Input selection of alogrithm you want to run" );
 				
 		int sampleCount = 0;
-		int input = scanner.nextInt();
-		
-		
-		if (input >=3){
-			System.out.println("Input number of samples to approximate" );
-			sampleCount = scanner.nextInt();
+		String input = null;
+		int choice = 0;
+		try {
+			input = bufferRead.readLine();
+			choice = Integer.parseInt(input);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
-		scanner.close();
 		
 		
-		if(input == 1){
+		if (choice >=3){
+			System.out.println("Input number of samples to approximate" );
+			try {
+				input = bufferRead.readLine();
+				sampleCount = Integer.parseInt(input);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		if(choice == 1){
 			algorithm = new EnumerationAsk();
 		}
-		else if(input == 2){
+		else if(choice == 2){
 			algorithm = new EliminationAsk();
 		}
-		else if(input == 3){
+		else if(choice == 3){
 			algorithm = new BayesInferenceApproxAdapter(new RejectionSampling(), sampleCount);
 		}
-		else if(input == 4){
+		else if(choice == 4){
 			algorithm = new BayesInferenceApproxAdapter(new LikelihoodWeighting(), sampleCount);
 		}
-		else if(input == 5){
+		else if(choice == 5){
 			algorithm = new BayesInferenceApproxAdapter(new GibbsAsk(), sampleCount);
 		}
 		
@@ -129,10 +217,18 @@ public class BayesNetRunner {
 		}
 	}
 	
+	private static void printEvidenceList(List<RandomVariable> varList){
+		for (int i=0; i<varList.size(); i++){
+			RandomVariable temp = varList.get(i);
+			Domain type = temp.getDomain();
+			System.out.println(i + " " + temp.toString() + ", " + type.toString() );
+		}
+	}
+	
 	private static List<AssignmentProposition> getEvidence(List<RandomVariable> varList){
 		List<AssignmentProposition> evidenceList = new ArrayList<AssignmentProposition>();
 		BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-		printList(varList);
+		printEvidenceList(varList);
 		System.out.println("Separated by commas, input indexes and truth values for all the evidence variables");
 		
 		String delims = "[,]";
@@ -150,13 +246,9 @@ public class BayesNetRunner {
 		
 		for(int i=0; i<tokens.length; i+=2){
 			int temp =  Integer.parseInt(tokens[i]);
-			boolean truthValue = false;
 			String testValue = tokens[i+1];
-			if(testValue.contains("T") ||testValue.contains("t") || testValue.contains("true") )
-				truthValue = true;
-			else
-				truthValue = false;
-			AssignmentProposition assignment = new 	AssignmentProposition(varList.get(temp), truthValue);
+			
+			AssignmentProposition assignment = new 	AssignmentProposition(varList.get(temp), testValue);
 			evidenceList.add(assignment);
 		}
 		//
--- BayesNets/src/cpsc470/bayes/Utils.java
+++ BayesNets/src/cpsc470/bayes/Utils.java
@@ -1,10 +1,13 @@
 package cpsc470.bayes;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import aima.core.probability.CategoricalDistribution;
 import aima.core.probability.RandomVariable;
 import aima.core.probability.bayes.BayesianNetwork;
+import aima.core.probability.domain.Domain;
+import aima.core.probability.domain.FiniteDomain;
 
 public class Utils {
 
@@ -30,26 +33,84 @@ public class Utils {
 		
 		dist1RandVars = distrib1.getFor();
 		dist2RandVars = distrib2.getFor();
+
 		
 		if(!dist1RandVars.equals(dist2RandVars))
 			throw new IllegalArgumentException();
 		
 		int numVariables = dist1RandVars.size();
-		for(RandomVariable w : dist1RandVars)
+		int numValues = 1;
+		
+		// array holds variable names
+		Object varArr[] = dist1RandVars.toArray(); //(RandomVariable[])
+		// array holds domains corresponding to variables
+		FiniteDomain varDomainsArr[] = new FiniteDomain[varArr.length];
+		for(int variableI = 0; variableI < varArr.length; variableI++)
 		{
-			double probabilityw1 = distrib1.getValue(w);
-			double probabilityw2 = distrib2.getValue(w);
-			double difference = probabilityw1 - probabilityw2;
-			double differenceSquared = Math.pow(difference, 2);
+			varDomainsArr[variableI] = (FiniteDomain) ((RandomVariable) varArr[variableI]).getDomain();
+			numValues *= varDomainsArr[variableI].size();
+		}
+			
+		// initialize values to the first value in domain and calculate
+		Object valueArr[] = new Object[varArr.length];
+		for(int variableI = 0; variableI < varArr.length; variableI++)
+			valueArr[variableI] = varDomainsArr[variableI].getValueAt(0); 
+
+		double probabilityw1 = distrib1.getValue(valueArr);
+		double probabilityw2 = distrib2.getValue(valueArr);
+		double difference = probabilityw1 - probabilityw2;
+		double differenceSquared = Math.pow(difference, 2);
+		sumOfSquares += differenceSquared;
+
+		
+		// flip values to get every combinations of values possible and calculate
+		for(int valueCombo = 1; valueCombo < Math.pow(2, numVariables); valueCombo++) // Gets different combinations
+			// of values for the whole set of variables
+		{
+			byte[] comboInBits = toBytes(valueCombo);
+			int bitIndex = 0; // Bit index in each byte (7-0)
+			int arrayIndex = comboInBits.length - 1; // Byte index for each set of 8 bits
+			int bitValuesCovered = 0; // Keep track of how many values have changed
+			while (arrayIndex >= 0 && bitValuesCovered < numVariables) // Changes variable values according to bit values
+			{
+				if ((comboInBits[arrayIndex] >> (bitIndex) & 1) == 1)
+					valueArr[numVariables - bitValuesCovered - 1] = varDomainsArr[numVariables - bitValuesCovered - 1].getValueAt(1); 
+				else
+					valueArr[numVariables - bitValuesCovered - 1] = varDomainsArr[numVariables - bitValuesCovered - 1].getValueAt(0); 
+				bitIndex++;
+				bitValuesCovered++;
+				if (bitIndex == 8)
+				{
+					bitIndex = 0;
+					arrayIndex--;
+				}
+			}
+			
+			probabilityw1 = distrib1.getValue(valueArr);
+			probabilityw2 = distrib2.getValue(valueArr);
+			difference = probabilityw1 - probabilityw2;
+			differenceSquared = Math.pow(difference, 2);
 			sumOfSquares += differenceSquared;
 		}
 		
-		double mean = sumOfSquares / (double) numVariables;
+		double mean = sumOfSquares / (double) numValues;
 		rmse = Math.sqrt(mean);
 		
 		return rmse;
 	}
 
+	protected static byte[] toBytes(int n) {
+		byte[] intByte = new byte[4];
+		
+		for(int i = intByte.length-1; i >= 0; i--)
+		{
+			intByte[i] = (byte) n;
+			n >>= 8;
+		}
+		
+		return intByte;
+	}
+	
 	/**
 	 * Simple O(n) search for random variable in Bayesian network with given name.
 	 * @param name
